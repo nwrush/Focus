d9ae5daacbea3be4458d5f60474d83f09c983831
Nikko Rush <nikko.rush@gmail.com>
2014-11-08 14:32:28 -0800

    Entity

--- Project Focus/Project_Focus/entities/Entity.cs
+++ Project Focus/Project_Focus/entities/Entity.cs
@@ -5,22 +5,48 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Project_Focus.entities {
-    class Entity {
-        public Entity() {
+namespace Focus.entities
+{
+    class Entity
+    {
+        protected Vector2 position;
+        protected Vector2 speed;
+        protected Texture2D texture;
+
+        //draw the entity to the screen
+        bool isDead;
+
+        public Entity()
+        {
+            isDead = false;
+        }
+        public Entity(Vector2 pos, Vector2 speed)
+            : this()
+        {
+            this.position = pos;
+            this.speed = speed;
+        }
+
+        public virtual void LoadContent(string contentName)
+        {
+            
+        }
+
+        public virtual void Update()
+        {
 
         }
 
-        public virtual void LoadContent() {
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
 
         }
 
-        public virtual void Update() {
-
-        }
-
-        public virtual void Draw(SpriteBatch spriteBatch) {
-
+        //Accessors
+        public Boolean Dead
+        {
+            public get { return this.isDead; }
+            public set { this.isDead = value; }
         }
 
     }
