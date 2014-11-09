0aa16c5595fb81e91a34ad6509e8eda84d4ca8fa
Alex Hoar <thealexhoar@gmail.com>
2014-11-08 22:39:37 -0800

    button class

--- Project Focus/Project_Focus/entities/Button.cs
+++ Project Focus/Project_Focus/entities/Button.cs
@@ -9,16 +9,57 @@ using Focus.globals;
 
 namespace Focus.entities {
     class Button:Entity {
-        Texture2D hoverTexture, clickTexture;
-        public Button(string baseName)
+        Texture2D baseTexture, hoverTexture, clickTexture;
+        Rectangle buttonRect;
+        bool hovered = false;
+        bool clicked = false;
+        public bool clickReleased = false;
+        
+        public Button(string baseName, Vector2 pos)
             : base(baseName) {
+                position = pos;
+                buttonRect = new Rectangle(285, 285, 285, 35);
         }
 
         protected virtual void LoadContent(string contentName)
         {
-           texture = GV.contentManager.Load<Texture2D>(contentName);
+           baseTexture = GV.contentManager.Load<Texture2D>(contentName + "_base");
            hoverTexture = GV.contentManager.Load<Texture2D>(contentName + "_hover");
            clickTexture = GV.contentManager.Load<Texture2D>(contentName + "_click");
+           texture = baseTexture;
+        }
+
+        public override void Update() {
+            clicked = false;
+            hovered = false;
+            clickReleased = false;
+
+            if ((Input.getPos().X >= buttonRect.X 
+                || Input.getPos().X <= buttonRect.Right) 
+                || (Input.getPos().Y >= buttonRect.Y 
+                || Input.getPos().Y <= buttonRect.Bottom)) {
+                    if (Input.isLeftMouseDown()) {
+                        clicked = true;
+                    }
+                    else if (Input.isLeftMouseReleased()) {
+                        clickReleased = true;
+                    }
+                    else {
+                        hovered = true;
+                    }
+            }
+
+            if (clicked) {
+                texture = clickTexture;
+            }
+            else if (hovered) {
+                texture = hoverTexture;
+            }
+            else {
+                texture = baseTexture;
+            }
+
+            
         }
     }
 }
